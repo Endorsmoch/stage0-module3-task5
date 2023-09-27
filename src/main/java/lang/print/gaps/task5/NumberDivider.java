8dece5608bca9d94de7ad1bddb418cfcb4ed3334
Endorsmoch <a16003872@alumnos.uady.mx>
2023-09-27 11:41:11 -0600

    Finished

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -1,6 +1,8 @@
 package lang.print.gaps.task5;
 
 public class NumberDivider {
-    void divide(int toBeDivided) {
+    public void divide(int toBeDivided) {
+        float result = (float) toBeDivided/5;
+        System.out.println(result);
     }
 }
